a8e78810ece944a211e4f9810de8f8558f715f10
hwanginbeom <rydn2004@naver.com>
2018-08-30 07:09:06 +0000

    CRUD-Update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request ,redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from model import *
@@ -41,11 +41,34 @@ def read(id):
     #SELECT * FROM posts WHERE id = 1;
     return render_template("read.html",post=post )
     
-@app.route("/posts/delete/<int:id>")
+@app.route("/posts/<int:id>/delete")
 def delete(id):
     #DB에서 특정한 게시글(id)을 가져와 !
     post = Post.query.get(id) # Post 클래스의 메소드라고 생각하는게 편하다
     #SELECT * FROM posts WHERE id = 1;
     db.session.delete(post)
     db.session.commit()
-    return render_template("delete.html", post=post )
+    #DELETE FROM posts WHERE id=2
+    return redirect('/')
+    #return render_template('index.html') 이거랑 다른 이유는 이렇게하면 그저 index.html을 형태를 호출 할 뿐 url의 주소는 /posts/<int:id>/delete
+
+    #return render_template("delete.html", post=post ) /이렇게 하면 ure
+
+@app.route("/posts/<int:id>/edit")
+def edit(id):
+    post = Post.query.get(id) # Post 클래스의 메소드라고 생각하는게 편하다
+    #SELECT * FROM posts WHERE id = 1;
+    return render_template("edit.html",post=post )
+   
+@app.route("/posts/<int:id>/update" , methods=["POST"])
+def update(id):
+    #DB에서 특정한 게시글(id)을 가져와 !
+    post = Post.query.get(id) # Post 클래스의 메소드라고 생각하는게 편하다
+    #SELECT * FROM posts WHERE id = 1;
+    post.title = request.form.get('title')
+    post.content = request.form.get('content')
+    db.session.commit()
+    #UPDATAE posts SET title = "hihi"
+    #WHERE id = 2;
+    return redirect("/posts/{}".format(post.id))
+   
